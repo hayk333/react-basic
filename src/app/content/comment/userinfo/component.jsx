54ca86cb66d6b2fdd05b758425101ca140657813
RockStar <gagik.aproyan.dev@gmail.com>
2020-02-08 12:52:06 +0400

    form with select

--- src/app/content/comment/userinfo/component.jsx
+++ src/app/content/comment/userinfo/component.jsx
@@ -2,21 +2,51 @@ import React from 'react';
 
 import './component.css';
 
-function UserInfo(props) {
+class UserInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: ''
+        }
+    }
 
-    console.log('es gtnvum em userInfo-i mej', props);
-    
-    return (
-        <div className="UserInfo">
-            <img className="Avatar"
-                src={props.author.avatarUrl}
-                alt={props.author.name}
-            />
-            <div className="UserInfo-name">
-                {props.author.name}
+    handleChange = (event) => {
+        this.setState({
+            value: event.target.value
+        });
+    }
+
+    handleSubmit = (event) => {
+        console.log(this.state.value);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div className="UserInfo">
+                <img className="Avatar"
+                    src={this.props.author.avatarUrl}
+                    alt={this.props.author.name}
+                />
+                <div className="UserInfo-name">
+                    {this.props.author.name}
+                </div>
+
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name:
+                        <select value={this.state.value} onChange={this.handleChange}>
+                            <option value='apple'>apple</option>
+                            <option value='mango'>mango</option>
+                            <option value='banan'>banan</option>
+                            <option value='ananas'>ananas</option>
+                        </select>
+                    </label>
+                    <button type='submit'>send</button>
+                </form>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default UserInfo;
